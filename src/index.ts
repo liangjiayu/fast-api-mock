0538bb1b4a178d88b250e46607d1e99f2faeb7e1
liangjiayu <1013834609@qq.com>
2026-03-28 20:49:55 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono123!')
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+export default app
